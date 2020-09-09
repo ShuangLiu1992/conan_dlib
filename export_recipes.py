94656404fec8a5e4c314e63f4059c3ea05711a34
ShuangLiu <liushuang.work@gmail.com>
2020-09-09 21:30:43 +0100

    change folder name

--- export_recipes.py
+++ export_recipes.py
@@ -2,4 +2,4 @@ from conans.client.conan_api import Conan
 
 conan_api, _, _ = Conan.factory()
 
-conan_api.export("./dlib", "dlib", "19.21", "shuangliu1992", "travis")
+conan_api.export("./recipe", "dlib", "19.21", "shuangliu1992", "travis")
