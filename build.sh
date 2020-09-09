af717effc112bd0e52f7b3674d3d17c3106e7bcd
ShuangLiu <liushuang.work@gmail.com>
2020-09-09 22:02:49 +0100

    it's not actually secure....

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ conan profile new profile --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan export recipe dlib/19.21@shuangliu1992/travis
 cd build
-conan install . --build=outdated
-conan user -p $CONAN_PASSWORD -r https://bintray.com/shuangliu1992 $CONAN_LOGIN_USERNAME
+#conan install . --build=outdated
+conan remote add bintray https://bintray.com/$CONAN_LOGIN_USERNAME
+conan user -p $CONAN_PASSWORD -r bintray $CONAN_LOGIN_USERNAME
 conan remote list
