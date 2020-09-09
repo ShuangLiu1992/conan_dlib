2f04f4e62a4acf97e6aadda491f61f86e8e06ad4
ShuangLiu <liushuang.work@gmail.com>
2020-09-09 21:43:37 +0100

    change order

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cd build
 conan user
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan export recipe dlib/19.21@shuangliu1992/travis
+cd build
 conan install . --build=outdated
