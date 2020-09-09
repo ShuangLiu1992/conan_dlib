21fffe80ff766ef5680693a6572f556b55a5c22a
ShuangLiu <liushuang.work@gmail.com>
2020-09-09 22:04:40 +0100

    upload to bintray

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ conan profile new profile --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan export recipe dlib/19.21@shuangliu1992/travis
 cd build
-#conan install . --build=outdated
+conan install . --build=outdated
 conan remote add bintray https://bintray.com/$CONAN_LOGIN_USERNAME
 conan user -p $CONAN_PASSWORD -r bintray $CONAN_LOGIN_USERNAME
 conan remote list
+conan upload "*" --all --confirm -r=bintray
