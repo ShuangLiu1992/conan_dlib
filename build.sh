5241601d47bc58435d72e7b84b6953d9b6542455
ShuangLiu <liushuang.work@gmail.com>
2020-09-09 22:17:52 +0100

    it should be uploaded to root

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ conan profile update settings.compiler.libcxx=libstdc++11 default
 conan export recipe dlib/19.21@$CONAN_LOGIN_USERNAME/travis
 cd build
 conan install . --build=outdated
-conan remote add bintray https://api.bintray.com/conan/$CONAN_LOGIN_USERNAME/test 
+conan remote add bintray https://api.bintray.com/conan/$CONAN_LOGIN_USERNAME 
 conan user -p $CONAN_PASSWORD -r bintray $CONAN_LOGIN_USERNAME
 conan remote list
 conan upload "*" --all --confirm -r=bintray
