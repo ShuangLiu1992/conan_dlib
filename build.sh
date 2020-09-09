790bfee7458adacc11a797775ded4baa9a930be0
ShuangLiu <liushuang.work@gmail.com>
2020-09-09 21:36:41 +0100

    generate default profile

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 python3 export_recipes.py
 cd build
+conan user
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan export recipe dlib/19.21@shuangliu1992/travis
 conan install . --build=outdated
