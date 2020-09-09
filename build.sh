67b0e5d9ea91eef3b00ba6a337b77b530c2d15cf
ShuangLiu <liushuang.work@gmail.com>
2020-09-09 22:25:03 +0100

    bionic

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 conan user
-conan profile new profile --detect
-conan profile update settings.compiler.libcxx=libstdc++11 default
 conan export recipe dlib/19.21@$CONAN_LOGIN_USERNAME/travis
 cd build
 conan install . --build=outdated
-conan remote add bintray https://api.bintray.com/conan/$CONAN_LOGIN_USERNAME 
+conan remote add bintray https://api.bintray.com/conan/$CONAN_LOGIN_USERNAME/test 
 conan user -p $CONAN_PASSWORD -r bintray $CONAN_LOGIN_USERNAME
 conan remote list
 conan upload "*" --all --confirm -r=bintray
