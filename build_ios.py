bd2d666ab344a160d4190065b2bae2b96c8bbd46
ShuangLiu <liushuang.work@gmail.com>
2020-09-10 11:51:45 +0100

    try ios

---
+++ build_ios.py
@@ -0,0 +1,22 @@
+from conans.client.conan_api import Conan
+import platform
+import os
+
+
+conan_api, _, _ = Conan.factory()
+
+conan_api.create_profile("default", detect=True)
+if platform.system() == "Linux":
+    conan_api.update_profile("default", "settings.compiler.libcxx", "libstdc++11")
+conan_api.config_set("general.cmake_generator", "Xcode")
+
+CONAN_LOGIN_USERNAME = os.getenv('CONAN_LOGIN_USERNAME')
+CONAN_PASSWORD = os.getenv('CONAN_PASSWORD')
+
+conan_api.remote_add("bintray", f"https://api.bintray.com/conan/{CONAN_LOGIN_USERNAME}/test")
+conan_api.authenticate(CONAN_LOGIN_USERNAME, CONAN_PASSWORD, "bintray")
+
+conan_api.export("./ios_recipe", "dlib", "19.21", CONAN_LOGIN_USERNAME, "github_action")
+
+conan_api.install("./build", build=["outdated"])
+conan_api.upload("*", all_packages=True, confirm=True, remote_name="bintray")
---
+++ ios_recipe/conanfile.py
@@ -0,0 +1,76 @@
+from conans import ConanFile, CMake, tools
+
+import os
+import platform
+import copy
+
+class DLIBConan(ConanFile):
+    name = "dlib"
+    version = "19.21"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Hello here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False], "build_archs": "ANY"}
+    default_options = {"shared": False, "fPIC": True, "build_archs": "x86,x86_64"}
+    generators = "cmake"
+    exports_sources = "toolchain.cmake"
+
+    def package(self):
+        self.copy("toolchain.cmake")
+
+    def package_info(self):
+        self.env_info.CONAN_CMAKE_TOOLCHAIN_FILE = os.path.join(self.package_folder, "toolchain.cmake")
+
+    def configure(self):
+        self.output.error(self.options.build_archs)
+
+
+    def init(self):
+        self.source_path = f"{self.name}-{self.version}"
+
+    def source(self):
+        md5s = {}
+        md5s["19.21"] = "a521a8a4d7aaba13465fbbc217bc4322"
+        md5s["19.18"] = "3cf2a5b7a40fd61834dcb86c57638f38"
+        tools.download(f"https://github.com/davisking/dlib/archive/v{self.version}.tar.gz", "src.tar.gz",
+                       md5=md5s[self.version])
+
+        tools.unzip("src.tar.gz")
+        tools.replace_in_file(f"{self.source_path}/dlib/CMakeLists.txt", 'project(dlib)',
+                              '''
+                              project(dlib)
+                              include(${CMAKE_BINARY_DIR}/../conanbuildinfo.cmake)
+                              conan_basic_setup()
+                              ''')
+
+    def configure_cmake(self):
+        # dlib install exclude build folder with regex pattern REGEX "${CMAKE_CURRENT_BINARY_DIR}" EXCLUDE)
+        self.build_folder = self.build_folder + "/build"
+        cmake = CMake(self)
+        cmake.definitions['DLIB_USE_CUDA'] = "OFF"
+        cmake.definitions['DLIB_ENABLE_STACK_TRACE'] = "OFF"
+        cmake.definitions['DLIB_NO_GUI_SUPPORT'] = "ON"
+        cmake.definitions['DLIB_USE_MKL_FFT'] = "OFF"
+        cmake.definitions['DLIB_USE_BLAS'] = "OFF"
+        cmake.definitions['DLIB_USE_LAPACK'] = "OFF"
+        cmake.definitions['DLIB_PNG_SUPPORT'] = "OFF"
+        cmake.definitions['DLIB_GIF_SUPPORT'] = "OFF"
+        cmake.definitions['DLIB_JPEG_SUPPORT'] = "OFF"
+        cmake.definitions['DLIB_ENABLE_ASSERTS'] = "OFF"
+        cmake.definitions['DLIB_LINK_WITH_SQLITE3'] = "OFF"
+
+        cmake.definitions['CMAKE_TOOLCHAIN_FILE'] = os.path.join(self.source_folder, "toolchain.cmake")
+
+        cmake.configure(source_folder=f"{self.source_path}/dlib")
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["dlib"]
